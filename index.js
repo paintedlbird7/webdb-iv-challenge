d2e772fb020a1b1b4e7aa8d229b60164563628d4
paintedlbird7 <rosaperezlarios@gmail.com>
2019-04-11 13:44:04 -0700

    created knexfile & added dependencies

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 const server = require('./api/server.js');
 
 
+// recipe = tracks
+// ingredient = students
+// dish = cohort
+
+
+
 const port = process.env.PORT || 8000;
 server.listen(port, () =>
   console.log(`\n** API running on http://localhost:${port} **\n`)
