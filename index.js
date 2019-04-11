7425e45453fb0e87e53de5a83bbb3fed9e34ff84
paintedlbird7 <rosaperezlarios@gmail.com>
2019-04-11 12:28:56 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Manage Recipes (id, name)
+const server = require('./api/server.js');
+
+
+const port = process.env.PORT || 8000;
+server.listen(port, () =>
+  console.log(`\n** API running on http://localhost:${port} **\n`)
+);
